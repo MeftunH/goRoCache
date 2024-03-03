1430b7b7d4721e7bd8f113fc7d7ecf98756a031c
MeftunH <maftunhashimli@gmail.com>
2024-03-03 23:24:27 +0300

    FEAT: set map-cache structure

--- mapCache.go
+++ mapCache.go
@@ -2,9 +2,22 @@ package goRoCache
 
 import (
 	_ "fmt"
+	"sync"
 	_ "time"
 )
 
 type cacheChannel struct {
 	stopChannel chan bool
+}
+type mapCache struct {
+	// Holds the key/values in the cache
+	cacheMap map[interface{}]interface{}
+
+	// Holds the channels that stop the auto removal routines.
+	removeChannels map[interface{}]*cacheChannel
+
+	// Holds the channels that stop the auto update routines.
+	updateChannels map[interface{}]*cacheChannel
+
+	mutex sync.Mutex
 }
