4e82270ac7a351a56d66027ce6c9fba5bdabf97a
MeftunH <maftunhashimli@gmail.com>
2024-03-03 23:39:46 +0300

    TEST: add Test New Error With Nil Nested Error unit test

--- mapCache_test.go
+++ mapCache_test.go
@@ -49,3 +49,20 @@ func TestNewMapCacheNonNilFields(t *testing.T) {
 		t.Errorf("updateChannels field is nil")
 	}
 }
+func TestNewErrorWithNilNestedError(t *testing.T) {
+	errType := errorType("test")
+	msg := "test message"
+	result := newError(errType, msg)
+
+	if result.nestedError != nil {
+		t.Errorf("Expected nestedError to be nil, but got %v", result.nestedError)
+	}
+
+	if result.msg != msg {
+		t.Errorf("Expected msg to be %s, but got %s", msg, result.msg)
+	}
+
+	if result.errType != errType {
+		t.Errorf("Expected errType to be %s, but got %s", errType, result.errType)
+	}
+}
