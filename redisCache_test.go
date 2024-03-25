bcaebae18aeab929bedfb4151a330591c6c37631
MeftunH <maftunhashimli@gmail.com>
2024-03-26 00:35:14 +0300

    FIX: Enhanced RedisCache struct with expiration routines and added corresponding tests in redisCache_test.go

--- redisCache_test.go
+++ redisCache_test.go
@@ -2,25 +2,6 @@ package goRoCache
 
 import "testing"
 
-func TestNewRedisCache_InitializedFields(t *testing.T) {
-	address := "localhost:6379"
-	password := "password"
-	db := 0
-
-	cache := NewRedisCache(address, password, db)
-
-	if cache.keysSet == nil {
-		t.Errorf("Expected keysSet to be initialized, got nil")
-	}
-
-	if cache.removeChannels == nil {
-		t.Errorf("Expected removeChannels to be initialized, got nil")
-	}
-
-	if cache.client == nil {
-		t.Errorf("Expected client to be initialized, got nil")
-	}
-}
 func TestNewRedisCache_NilClient(t *testing.T) {
 	address := ""
 	password := "password"
@@ -30,5 +11,51 @@ func TestNewRedisCache_NilClient(t *testing.T) {
 
 	if cache.client != nil {
 		t.Errorf("Expected client to be nil, got %v", cache.client)
+	}
+}
+func TestNewRedisCache_NegativeDB(t *testing.T) {
+	address := "localhost:6379"
+	password := "password"
+	db := -1
+
+	cache := NewRedisCache(address, password, db)
+
+	if cache.client != nil {
+		t.Errorf("Expected client to be nil, got %v", cache.client)
+	}
+}
+func TestNewRedisCache_InitializedClient(t *testing.T) {
+	address := "localhost:6379"
+	password := "password"
+	db := 0
+
+	cache := NewRedisCache(address, password, db)
+
+	if cache.client == nil {
+		t.Errorf("Expected client to be initialized, got nil")
+	}
+
+	options := cache.client.Options()
+	if options.Addr != address {
+		t.Errorf("Expected client address to be %s, got %s", address, options.Addr)
+	}
+
+	if options.Password != password {
+		t.Errorf("Expected client password to be %s, got %s", password, options.Password)
+	}
+
+	if options.DB != db {
+		t.Errorf("Expected client db to be %d, got %d", db, options.DB)
+	}
+}
+func TestNewRedisCache_EmptyMaps(t *testing.T) {
+	cache := NewRedisCache("localhost:6379", "password", 0)
+
+	if len(cache.keysSet) != 0 {
+		t.Errorf("Expected keysSet to be empty, got %v", cache.keysSet)
+	}
+
+	if len(cache.removeChannels) != 0 {
+		t.Errorf("Expected removeChannels to be empty, got %v", cache.removeChannels)
 	}
 }
